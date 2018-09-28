c874039c72e2d9413615b90edcdf1f28499f918f
Anton Kikin <a.kikin@tano-systems.com>
2018-09-28 14:01:06 +0300

    mstpd: Add init scripts for OpenWrt
    Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

---
+++ recipes-networking/mstpd/mstpd/files/mstpd.sh
@@ -0,0 +1,348 @@
+##########################################################################
+#
+# Copyright (C) 2018, Tano Systems,
+# Anton Kikin <a.kikin@tano-systems.com>
+#
+##########################################################################
+
+. "$IPKG_INSTROOT/lib/functions.sh"
+. "$IPKG_INSTROOT/lib/functions/network.sh"
+. "$IPKG_INSTROOT/usr/share/libubox/jshn.sh"
+
+# The mstpctl executable
+MSTPCTL_BIN='/sbin/mstpctl'
+
+# The mstpd executable
+MSTPD_BIN='/sbin/mstpd'
+
+# Tag for log
+MSTPD_LOG_TAG="mstpd"
+
+function mstpd_log_error()
+{
+	logger -p error -s -t "$MSTPD_LOG_TAG" $*
+}
+
+function mstpd_log_debug()
+{
+	logger -p debug -t "$MSTPD_LOG_TAG" $*
+}
+
+function mstpd_log_info()
+{
+	logger -p info -t "$MSTPD_LOG_TAG" $*
+}
+
+mstpd_sanity_checks()
+{
+	if [ ! -x "$MSTPCTL_BIN" ]; then
+		mstpd_log_error "lib: 'mstpctl' binary does not exist or is not executable"
+		exit 1
+	fi
+
+	if [ ! -x "$MSTPD_BIN" ]; then
+		mstpd_log_error "lib: 'mstpd' binary does not exist or is not executable"
+		exit 1
+	fi
+
+	# Make sure this script is being run as root.
+	if [ "$(id -u)" != '0' ]; then
+		mstpd_log_error 'lib: script must be run as root'
+		exit 1
+	fi
+}
+
+##########################################################################
+#
+# Configure bridge port by mstpctl tool
+#
+# Arguments:
+#   $1 - bridge netifd interface name
+#   $2 - bridge system interface name
+#   $3 - port system interface name
+#
+# Example:
+#   mstpd_configure_port lan br-lan sw1p1
+#
+##########################################################################
+mstpd_configure_port()
+{
+	local br_ifname="$1"
+	local br_sysname="$2"
+	local br_port="$3"
+
+	config_load 'mstpd'
+	config_get mstpctl_portpathcost  "$br_ifname" "port_${br_port}_pathcost"
+	config_get mstpctl_portadminedge "$br_ifname" "port_${br_port}_adminedge"
+	config_get mstpctl_portautoedge  "$br_ifname" "port_${br_port}_autoedge"
+	config_get mstpctl_portp2p       "$br_ifname" "port_${br_port}_p2p"
+	config_get mstpctl_portbpduguard "$br_ifname" "port_${br_port}_bpduguard"
+	config_get mstpctl_portrestrrole "$br_ifname" "port_${br_port}_restrrole"
+	config_get mstpctl_portrestrtcn  "$br_ifname" "port_${br_port}_restrtcn"
+	config_get mstpctl_portnetwork   "$br_ifname" "port_${br_port}_network"
+	config_get mstpctl_porttreeprio  "$br_ifname" "port_${br_port}_treeprio"
+	config_get mstpctl_porttreecost  "$br_ifname" "port_${br_port}_treecost"
+
+	if [ "$mstpctl_portpathcost" ]; then
+		${MSTPCTL_BIN} setportpathcost "$br_sysname" "$br_port" "$mstpctl_portpathcost"
+		mstpd_log_debug "lib: port '$br_sysname:$br_port': setportpathcost $mstpctl_portpathcost"
+	fi
+
+	if [ "$mstpctl_portadminedge" ]; then
+		${MSTPCTL_BIN} setportadminedge "$br_sysname" "$br_port" "$mstpctl_portadminedge"
+		mstpd_log_debug "lib: port '$br_sysname:$br_port': setportadminedge $mstpctl_portadminedge"
+	fi
+
+	if [ "$mstpctl_portautoedge" ]; then
+		${MSTPCTL_BIN} setportautoedge "$br_sysname" "$br_port" "$mstpctl_portautoedge"
+		mstpd_log_debug "lib: port '$br_sysname:$br_port': setportautoedge $mstpctl_portautoedge"
+	fi
+
+	if [ "$mstpctl_portp2p" ]; then
+		${MSTPCTL_BIN} setportp2p "$br_sysname" "$br_port" "$mstpctl_portp2p"
+		mstpd_log_debug "lib: port '$br_sysname:$br_port': setportp2p $mstpctl_portp2p"
+	fi
+
+	if [ "$mstpctl_portbpduguard" ]; then
+		${MSTPCTL_BIN} setbpduguard "$br_sysname" "$br_port" "$mstpctl_portbpduguard"
+		mstpd_log_debug "lib: port '$br_sysname:$br_port': setbpduguard $mstpctl_portbpduguard"
+	fi
+
+	if [ "$mstpctl_portrestrrole" ]; then
+		${MSTPCTL_BIN} setportrestrrole "$br_sysname" "$br_port" "$mstpctl_portrestrrole"
+		mstpd_log_debug "lib: port '$br_sysname:$br_port': setportrestrrole $mstpctl_portrestrrole"
+	fi
+
+	if [ "$mstpctl_portrestrtcn" ]; then
+		${MSTPCTL_BIN} setportrestrtcn "$br_sysname" "$br_port" "$mstpctl_portrestrtcn"
+		mstpd_log_debug "lib: port '$br_sysname:$br_port': setportrestrtcn $mstpctl_portrestrtcn"
+	fi
+
+	if [ "$mstpctl_portnetwork" ]; then
+		${MSTPCTL_BIN} setportnetwork "$br_sysname" "$br_port" "$mstpctl_portnetwork"
+		mstpd_log_debug "lib: port '$br_sysname:$br_port': setportnetwork $mstpctl_portnetwork"
+	fi
+
+	if [ "$mstpctl_porttreeprio" ]; then
+		${MSTPCTL_BIN} settreeportprio "$br_sysname" "$br_port" 0 "$mstpctl_porttreeprio"
+		mstpd_log_debug "lib: port '$br_sysname:$br_port': settreeportprio $mstpctl_porttreeprio"
+	fi
+
+	if [ "$mstpctl_porttreecost" ]; then
+		${MSTPCTL_BIN} settreeportcost "$br_sysname" "$br_port" 0 "$mstpctl_porttreecost"
+		mstpd_log_debug "lib: port '$br_sysname:$br_port': settreeportcost $mstpctl_porttreecost"
+	fi
+
+	if [ "$mstpctl_portpathcost" ]; then
+		${MSTPCTL_BIN} setportpathcost "$br_sysname" "$br_port" "$mstpctl_portpathcost"
+		mstpd_log_debug "lib: port '$br_sysname:$br_port': setportpathcost $mstpctl_portpathcost"
+	fi
+}
+
+##########################################################################
+#
+# Configure bridge by mstpctl tool
+#
+# Arguments:
+#   $1 - bridge system interface name
+#
+# Example:
+#   mstpd_configure_bridge lan
+#
+##########################################################################
+mstpd_configure_bridge()
+{
+	local br_sysname="$1"
+	local br_ifname=${br_sysname/br-/}
+
+	# Check that interfaces is really bridge
+	if [ ! -d "/sys/class/net/$br_sysname/bridge" ]; then
+		${MSTPCTL_BIN} delbridge "$br_sysname"
+		mstpd_log_info "lib: bridge '$br_sysname' deleted (not a bridge)"
+		return 1
+	fi
+
+	local if_type="$(uci_get network.${br_ifname}.type)"
+	if [ "$if_type" != "bridge" ]; then
+		${MSTPCTL_BIN} delbridge "$br_sysname"
+		mstpd_log_info "lib: bridge '$br_sysname' deleted (not a bridge)"
+		return 1
+	fi
+
+	# Check that bridge has enabled STP
+	local if_stp="$(uci_get network.${br_ifname}.stp)"
+	if [ "$if_stp" != "1" ] && [ "$if_stp" != "on" ]; then
+		${MSTPCTL_BIN} delbridge "$br_sysname"
+		mstpd_log_info "lib: bridge '$br_sysname' deleted (has no enabled STP)"
+		return 1
+	fi
+
+	config_load 'mstpd'
+	config_get mstpctl_forcevers   "$br_ifname" 'forcevers'
+	config_get mstpctl_maxwait     "$br_ifname" 'maxwait'
+	config_get mstpctl_ageing      "$br_ifname" 'ageing'
+	config_get mstpctl_maxage      "$br_ifname" 'maxage'
+	config_get mstpctl_treeprio    "$br_ifname" 'treeprio'
+	config_get mstpctl_fdelay      "$br_ifname" 'fdelay'
+	config_get mstpctl_maxhops     "$br_ifname" 'maxhops'
+	config_get mstpctl_txholdcount "$br_ifname" 'txholdcount'
+	config_get mstpctl_hello       "$br_ifname" 'hello'
+
+	${MSTPCTL_BIN} addbridge "$br_sysname"
+	mstpd_log_debug "lib: bridge '$br_sysname' added"
+
+	current_fdelay=`${MSTPCTL_BIN} showbridge "$br_sysname" bridge-forward-delay`
+	current_maxage=`${MSTPCTL_BIN} showbridge "$br_sysname" bridge-max-age`
+
+	if ! [ "$mstpctl_maxage" ]; then
+		mstpctl_maxage=$current_maxage
+	fi
+
+	if ! [ "$mstpctl_fdelay" ]; then
+		mstpctl_fdelay=$current_fdelay
+	fi
+
+	new=$((2 * ($mstpctl_fdelay - 1)))
+
+	if [ "$new" -ge "$current_maxage" ]; then
+		# (2 * (new_forward_delay - 1)) >= current_maxage
+		${MSTPCTL_BIN} setfdelay "$br_sysname" "$mstpctl_fdelay"
+		${MSTPCTL_BIN} setmaxage "$br_sysname" "$mstpctl_maxage"
+	else
+		# (2 * (new_forward_delay - 1)) < current_maxage
+		${MSTPCTL_BIN} setmaxage "$br_sysname" "$mstpctl_maxage"
+		${MSTPCTL_BIN} setfdelay "$br_sysname" "$mstpctl_fdelay"
+	fi
+
+	mstpd_log_debug "lib: bridge '$br_sysname': setmaxage $mstpctl_maxage"
+	mstpd_log_debug "lib: bridge '$br_sysname': setfdelay $mstpctl_fdelay"
+
+	if [ "$mstpctl_maxhops" ]; then
+		${MSTPCTL_BIN} setmaxhops "$br_sysname" "$mstpctl_maxhops"
+		mstpd_log_debug "lib: bridge '$br_sysname': setmaxhops $mstpctl_maxhops"
+	fi
+
+	if [ "$mstpctl_txholdcount" ]; then
+		${MSTPCTL_BIN} settxholdcount "$br_sysname" "$mstpctl_txholdcount"
+		mstpd_log_debug "lib: bridge '$br_sysname': settxholdcount $mstpctl_txholdcount"
+	fi
+
+	if [ "$mstpctl_forcevers" ]; then
+		${MSTPCTL_BIN} setforcevers "$br_sysname" "$mstpctl_forcevers"
+		mstpd_log_debug "lib: bridge '$br_sysname': setforcevers $mstpctl_forcevers"
+	fi
+
+	if [ "$mstpctl_treeprio" ]; then
+		${MSTPCTL_BIN} settreeprio "$br_sysname" 0 "$mstpctl_treeprio"
+		mstpd_log_debug "lib: bridge '$br_sysname': settreeprio $mstpctl_treeprio"
+	fi
+
+	if [ "$mstpctl_hello" ]; then
+		${MSTPCTL_BIN} sethello "$br_sysname" "$mstpctl_hello"
+		mstpd_log_debug "lib: bridge '$br_sysname': sethello $mstpctl_hello"
+	fi
+
+	if [ "$mstpctl_ageing" ]; then
+		${MSTPCTL_BIN} setageing "$br_sysname" "$mstpctl_ageing"
+		mstpd_log_debug "lib: bridge '$br_sysname': setageing $mstpctl_ageing"
+	fi
+
+	# Configure ports
+	bridge_ports=`ls /sys/class/net/$br_sysname/brif/`
+
+	for br_port in $bridge_ports; do
+		mstpd_configure_port "$br_ifname" "$br_sysname" "$br_port"
+		mstpd_log_info "lib: port '$br_sysname:$br_port' configured"
+	done
+
+	mstpd_log_info "lib: bridge '$br_sysname' configured"
+
+	return 0
+}
+
+##########################################################################
+#
+# Check that mstpd service is running
+#
+##########################################################################
+mstpd_is_running()
+{
+	if ! pidof mstpd >/dev/null; then
+		return 1
+	else
+		return 0
+	fi
+}
+
+##########################################################################
+#
+# Configure all controlled by mstpd bridges
+# by mstpctl tool
+#
+##########################################################################
+mstpd_configure()
+{
+	local only_bridge
+	local mstpd_enabled
+	local bridge
+	local bridges
+	local bridges_json
+
+	only_bridge="$1"
+
+	mstpd_enabled="$(uci_get mstpd.global.enabled)"
+
+	if [ "${mstpd_enabled}" = "0" ]; then
+		mstpd_log_error "lib: mstpd is disabled"
+		exit 2
+	fi
+
+	# Read the mstpd config
+	config_load 'mstpd'
+	config_get bridges 'global' 'bridge'
+
+	# Read currently added to mstpd bridges list
+	bridges_json="{\"bridges\":`${MSTPCTL_BIN} -f json showbridge`}"
+
+	if [ "$?" = "0" ]; then
+		local key keys
+		local bridge_current
+		local bridges_current
+
+		bridges_current=""
+
+		json_init
+		json_load $bridges_json
+
+		json_select "bridges"
+			json_get_keys keys
+			for key in $keys; do
+				json_select "$key"
+					json_get_vars bridge
+					append bridges_current "$bridge" " "
+				json_select ..
+			done
+		json_select ..
+
+		# Delete bridges that is not exists in mstpd config
+		for bridge_current in $bridges_current; do
+			# if bridge is not in mstpd config then
+			# delete bridge from mstpd
+			if ! list_contains bridges "$bridge_current"; then
+				${MSTPCTL_BIN} delbridge "$bridge_current"
+				mstpd_log_info "lib: bridge '$bridge_current' deleted (not controlled)"
+			fi
+		done
+	fi
+
+	# Configure/add bridges
+	if [ -z "$only_bridge" ]; then
+		# Configure all bridges
+		for bridge in $bridges; do
+			mstpd_configure_bridge ${bridge}
+		done
+	else
+		# Configure only specified bridge
+		mstpd_configure_bridge ${only_bridge}
+	fi
+}
--- recipes-networking/mstpd/mstpd_git.bb
+++ recipes-networking/mstpd/mstpd_git.bb
@@ -1,7 +1,7 @@
 #
 SUMMARY = "Multiple Spanning Tree Protocol Daemon"
 
-PR = "tano6"
+PR = "tano8"
 
 LICENSE = "GPLv2"
 LIC_FILES_CHKSUM = "file://LICENSE;md5=4325afd396febcb659c36b49533135d4 \
@@ -10,17 +10,19 @@ LIC_FILES_CHKSUM = "file://LICENSE;md5=4325afd396febcb659c36b49533135d4 \
 FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}/patches:${THISDIR}/${PN}/files:"
 
 SRC_URI = "\
-	git://github.com/tano-systems/mstpd.git;protocol=https;branch=master \
-	file://0001-Startup-delay-patch.patch \
+	git://github.com/tano-systems/mstpd.git;protocol=https;branch=batch-command \
 "
 
 PV = "0.0.6+git${SRCPV}"
-SRCREV = "64521ef59a274ee1e012d7f5c4389244c408db65"
+
+# 27.09.2018 ctl_main: Add batch command execution support
+SRCREV = "55f80e4c72595511c472a02803d75c48eb13cf2d"
 
 SRC_URI += "\
-	file://openwrt-mstpd.config \
-	file://openwrt-bridge-stp \
-	file://openwrt-mstp_config_bridge \
+	file://bridge-stp \
+	file://mstpd.config \
+	file://mstpd.sh \
+	file://mstpd.init \
 "
 
 S = "${WORKDIR}/git"
@@ -34,16 +36,30 @@ EXTRA_OECONF = "--sbindir=/sbin"
 
 do_install_append() {
 	if [ "${@bb.utils.contains('DISTRO_FEATURES', 'procd', 'true', 'false', d)}" = "true" ]; then
+
+		# Remove unused in OpenWrt files
 		rm -rf ${D}${sysconfdir}/network
 		rm -rf ${D}${sysconfdir}/bridge-stp.conf
 		rm -rf ${D}/usr/libexec/mstpctl-utils/*
 
+		# Install directories
 		install -dm 0755 ${D}${sysconfdir}/config
-		install -dm 0755 ${D}/usr/libexec/mstpctl-utils
+		install -dm 0755 ${D}${sysconfdir}/init.d
+		install -dm 0755 ${D}/lib/functions
 		install -dm 0755 ${D}/sbin
 
-		install -m 0644 ${WORKDIR}/openwrt-mstpd.config ${D}${sysconfdir}/config/mstpd
-		install -m 0755 ${WORKDIR}/openwrt-bridge-stp ${D}/sbin/bridge-stp
-		install -m 0755 ${WORKDIR}/openwrt-mstp_config_bridge ${D}/usr/libexec/mstpctl-utils/mstp_config_bridge
+		# Install files
+		install -m 0755 ${WORKDIR}/bridge-stp ${D}/sbin/bridge-stp
+		install -m 0644 ${WORKDIR}/mstpd.config ${D}${sysconfdir}/config/mstpd
+		install -m 0755 ${WORKDIR}/mstpd.init ${D}${sysconfdir}/init.d/mstpd
+		install -m 0755 ${WORKDIR}/mstpd.sh ${D}/lib/functions/mstpd.sh
 	fi
 }
+
+FILES_${PN} += "/lib/functions/"
+
+inherit openwrt-services
+
+OPENWRT_SERVICE_PACKAGES = "mstpd"
+OPENWRT_SERVICE_SCRIPTS_mstpd += "mstpd"
+OPENWRT_SERVICE_STATE_mstpd-mstpd ?= "enabled"
