tano-systems/meta-tanowrt

commit 27750c00b51283a8d810bad7bec5888ff6aa50c0

procd: Add psplash support
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

recipes-core/procd/procd/patches/0007-rcS-Add-psplash-support.patch
@@ -1,0 +1,150 @@
+From f6137cd20d85215523a489a4c2ba585fd295f816 Mon Sep 17 00:00:00 2001
+From: Anton Kikin <a.kikin@tano-systems.com>
+Date: Fri, 5 Jul 2019 04:19:07 +0300
+Subject: [PATCH] rcS: Add psplash support
+
+Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>
+---
+ CMakeLists.txt |  7 ++++++
+ rcS.c          | 77 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ 2 files changed, 84 insertions(+)
+
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index f2f9792..3bc810a 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -27,6 +27,13 @@ ENDIF()
+ 
+ SET(LIBS ubox ubus json-c blobmsg_json json_script)
+ 
++IF(PSPLASH_SUPPORT)
++	ADD_DEFINITIONS(-DPSPLASH_SUPPORT)
++	IF(PSPLASH_SCRIPT_MSG)
++		ADD_DEFINITIONS(-DPSPLASH_SCRIPT_MSG)
++	ENDIF()
++ENDIF()
++
+ IF(CGROUP_SUPPORT)
+ 	ADD_DEFINITIONS(-DCGROUP_SUPPORT)
+ 	SET(LIBS ${LIBS} cgroup)
+diff --git a/rcS.c b/rcS.c
+index c2e1abb..6ce023b 100644
+--- a/rcS.c
++++ b/rcS.c
+@@ -40,8 +40,60 @@ struct initd {
+ 	struct timespec ts_start;
+ 	char *file;
+ 	char *param;
++#if defined(PSPLASH_SUPPORT)
++	int is_boot;
++#endif
+ };
+ 
++#if defined(PSPLASH_SUPPORT)
++
++#ifndef PSPLASH_FIFO_PATH
++#define PSPLASH_FIFO_PATH "/tmp/psplash/psplash_fifo"
++#endif
++
++static int initd_scripts = 0;
++static int initd_completed = 0;
++
++static void psplash_write(const char *command)
++{
++	int pipe_fd;
++
++	if ((pipe_fd = open(PSPLASH_FIFO_PATH, O_WRONLY | O_NONBLOCK)) == -1)
++		return;
++
++	write(pipe_fd, command, strlen(command) + 1);
++}
++
++#if defined(PSPLASH_SCRIPT_MSG)
++
++static void psplash_update_initd_script(const char *message)
++{
++	char command[64];
++	snprintf(command, sizeof(command), "MSG Starting %s...", message);
++	command[sizeof(command) - 1] = 0;
++	psplash_write(command);
++}
++
++#endif
++
++static void psplash_update_progress(void)
++{
++	if (!initd_scripts)
++		return;
++
++	if (initd_completed < initd_scripts) {
++		char command[16];
++		int progress = (initd_completed * 100) / initd_scripts;
++		snprintf(command, sizeof(command), "PROGRESS %d", progress);
++		command[sizeof(command) - 1] = 0;
++		psplash_write(command);
++	}
++	else {
++		psplash_write("QUIT");
++	}
++}
++#endif
++
+ static void pipe_cb(struct ustream *s, int bytes)
+ {
+ 	struct initd *initd = container_of(s, struct initd, fd.stream);
+@@ -71,6 +123,12 @@ static void q_initd_run(struct runqueue *q, struct runqueue_task *t)
+ 	int pipefd[2];
+ 	pid_t pid;
+ 
++#if defined(PSPLASH_SUPPORT) && defined(PSPLASH_SCRIPT_MSG)
++
++	if (s->is_boot)
++		psplash_update_initd_script(s->file);
++#endif
++
+ 	clock_gettime(CLOCK_MONOTONIC_RAW, &s->ts_start);
+ 	DEBUG(2, "start %s %s \n", s->file, s->param);
+ 	if (pipe(pipefd) == -1) {
+@@ -118,6 +176,13 @@ static void q_initd_complete(struct runqueue *q, struct runqueue_task *p)
+ 		ts_res.tv_nsec += 1000000000;
+ 	}
+ 
++#if defined(PSPLASH_SUPPORT)
++	if (s->is_boot && (initd_scripts > 0)) {
++		++initd_completed;
++		psplash_update_progress();
++	}
++#endif
++
+ 	DEBUG(2, "stop %s %s - took %lu.%09lus\n", s->file, s->param, ts_res.tv_sec, ts_res.tv_nsec);
+ 	ustream_free(&s->fd.stream);
+ 	close(s->fd.fd.fd);
+@@ -147,6 +212,11 @@ static void add_initd(struct runqueue *q, char *file, char *param)
+ 	}
+ 	s->param = p;
+ 	s->file = f;
++
++#if defined(PSPLASH_SUPPORT)
++	s->is_boot = !strcmp(param, "boot");
++#endif
++
+ 	strcpy(s->param, param);
+ 	strcpy(s->file, file);
+ 	runqueue_task_add(q, &s->proc.task, false);
+@@ -170,6 +240,13 @@ static int _rc(struct runqueue *q, char *path, const char *file, char *pattern,
+ 		return -1;
+ 	}
+ 
++#if defined(PSPLASH_SUPPORT)
++	if (initd_scripts == 0) {
++		if (strcmp(param, "boot") == 0)
++			initd_scripts = gl.gl_pathc;
++	}
++#endif
++
+ 	for (j = 0; j < gl.gl_pathc; j++)
+ 		add_initd(q, gl.gl_pathv[j], param);
+ 
+-- 
+2.16.2.windows.1
+

recipes-core/procd/procd_git.bb
@@ -3,7 +3,7 @@
 # Copyright (C) 2018-2019 Anton Kikin <a.kikin@tano-systems.com>
 # Released under the MIT license (see COPYING.MIT for the terms)
 
-PR = "tano24"
+PR = "tano25"
 SUMMARY = "procd is the new OpenWrt process management daemon written in C"
 DESCRIPTION = "procd is VIRTUAL-RUNTIME-init_manager"
 HOMEPAGE = "http://wiki.openwrt.org/doc/techref/procd"
@@ -11,6 +11,8 @@ LICENSE = "BSD"
 LIC_FILES_CHKSUM = "file://procd.c;beginline=1;endline=13;md5=61e3657604f131a859b57a40f27a9d8e"
 SECTION = "base"
 DEPENDS = "libubox ubus json-c"
+
+PACKAGE_ARCH = "${MACHINE_ARCH}"
 
 FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}/patches:${THISDIR}/${PN}/files:"
 
@@ -28,10 +30,17 @@ SRC_URI += "\
 	file://0004-hotplug-Remove-dev-prefix-from-DEVNAME-variable.patch \
 	file://0005-hotplug-Completely-remove-hotplug-functionality.patch \
 	file://0006-service-Add-SCHED_IDLE-scheduler-policy-support.patch \
+	file://0007-rcS-Add-psplash-support.patch \
 "
 
-PACKAGECONFIG ??= "${@bb.utils.contains('COMBINED_FEATURES', 'cgroup', 'cgroup', '', d)}"
+PACKAGECONFIG ??= "\
+	${@bb.utils.contains('COMBINED_FEATURES', 'cgroup', 'cgroup', '', d)} \
+	${@bb.utils.contains('MACHINE_FEATURES', 'screen', 'psplash psplash-script-msg', '', d)} \
+"
+
 PACKAGECONFIG[cgroup] = "-DCGROUP_SUPPORT=1,,libcgroup"
+PACKAGECONFIG[psplash] = "-DPSPLASH_SUPPORT=1,,"
+PACKAGECONFIG[psplash-script-msg] = "-DPSPLASH_SCRIPT_MSG=1,,"
 
 # 17.04.2019
 # procd: copy the respawn property of new instance

commit 9c3f81a6ce64f2c5dd5d316495b2bbfb5a38e873

procd: Update psplash support patch
Fixes: 27750c0 ("procd: Add psplash support")
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

recipes-core/procd/procd/patches/0007-rcS-Add-psplash-support.patch
@@ -1,4 +1,4 @@
-From f6137cd20d85215523a489a4c2ba585fd295f816 Mon Sep 17 00:00:00 2001
+From 1a9f03b5339c08fd57dbfd8cfc701a0ddba77749 Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Fri, 5 Jul 2019 04:19:07 +0300
 Subject: [PATCH] rcS: Add psplash support
@@ -6,8 +6,8 @@ Subject: [PATCH] rcS: Add psplash support
 Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>
 ---
  CMakeLists.txt |  7 ++++++
- rcS.c          | 77 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- 2 files changed, 84 insertions(+)
+ rcS.c          | 78 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ 2 files changed, 85 insertions(+)
 
 diff --git a/CMakeLists.txt b/CMakeLists.txt
 index f2f9792..3bc810a 100644
@@ -28,10 +28,10 @@ index f2f9792..3bc810a 100644
  	ADD_DEFINITIONS(-DCGROUP_SUPPORT)
  	SET(LIBS ${LIBS} cgroup)
 diff --git a/rcS.c b/rcS.c
-index c2e1abb..6ce023b 100644
+index c2e1abb..f4d61d4 100644
 --- a/rcS.c
 +++ b/rcS.c
-@@ -40,8 +40,60 @@ struct initd {
+@@ -40,8 +40,62 @@ struct initd {
  	struct timespec ts_start;
  	char *file;
  	char *param;
@@ -73,17 +73,19 @@ index c2e1abb..6ce023b 100644
 +
 +static void psplash_update_progress(void)
 +{
++	char command[16];
++	int progress;
++
 +	if (!initd_scripts)
 +		return;
 +
-+	if (initd_completed < initd_scripts) {
-+		char command[16];
-+		int progress = (initd_completed * 100) / initd_scripts;
-+		snprintf(command, sizeof(command), "PROGRESS %d", progress);
-+		command[sizeof(command) - 1] = 0;
-+		psplash_write(command);
-+	}
-+	else {
++	progress = (initd_completed * 100) / initd_scripts;
++	snprintf(command, sizeof(command), "PROGRESS %d", progress);
++	command[sizeof(command) - 1] = 0;
++	psplash_write(command);
++
++	if (initd_completed >= initd_scripts) {
++		psplash_write("MSG"); /* Clear message */
 +		psplash_write("QUIT");
 +	}
 +}
@@ -92,12 +94,11 @@ index c2e1abb..6ce023b 100644
  static void pipe_cb(struct ustream *s, int bytes)
  {
  	struct initd *initd = container_of(s, struct initd, fd.stream);
-@@ -71,6 +123,12 @@ static void q_initd_run(struct runqueue *q, struct runqueue_task *t)
+@@ -71,6 +125,11 @@ static void q_initd_run(struct runqueue *q, struct runqueue_task *t)
  	int pipefd[2];
  	pid_t pid;
  
 +#if defined(PSPLASH_SUPPORT) && defined(PSPLASH_SCRIPT_MSG)
-+
 +	if (s->is_boot)
 +		psplash_update_initd_script(s->file);
 +#endif
@@ -105,7 +106,7 @@ index c2e1abb..6ce023b 100644
  	clock_gettime(CLOCK_MONOTONIC_RAW, &s->ts_start);
  	DEBUG(2, "start %s %s \n", s->file, s->param);
  	if (pipe(pipefd) == -1) {
-@@ -118,6 +176,13 @@ static void q_initd_complete(struct runqueue *q, struct runqueue_task *p)
+@@ -118,6 +177,13 @@ static void q_initd_complete(struct runqueue *q, struct runqueue_task *p)
  		ts_res.tv_nsec += 1000000000;
  	}
  
@@ -119,7 +120,7 @@ index c2e1abb..6ce023b 100644
  	DEBUG(2, "stop %s %s - took %lu.%09lus\n", s->file, s->param, ts_res.tv_sec, ts_res.tv_nsec);
  	ustream_free(&s->fd.stream);
  	close(s->fd.fd.fd);
-@@ -147,6 +212,11 @@ static void add_initd(struct runqueue *q, char *file, char *param)
+@@ -147,6 +213,11 @@ static void add_initd(struct runqueue *q, char *file, char *param)
  	}
  	s->param = p;
  	s->file = f;
@@ -131,7 +132,7 @@ index c2e1abb..6ce023b 100644
  	strcpy(s->param, param);
  	strcpy(s->file, file);
  	runqueue_task_add(q, &s->proc.task, false);
-@@ -170,6 +240,13 @@ static int _rc(struct runqueue *q, char *path, const char *file, char *pattern,
+@@ -170,6 +241,13 @@ static int _rc(struct runqueue *q, char *path, const char *file, char *pattern,
  		return -1;
  	}
  

recipes-core/procd/procd_git.bb
@@ -3,7 +3,7 @@
 # Copyright (C) 2018-2019 Anton Kikin <a.kikin@tano-systems.com>
 # Released under the MIT license (see COPYING.MIT for the terms)
 
-PR = "tano26"
+PR = "tano27"
 SUMMARY = "procd is the new OpenWrt process management daemon written in C"
 DESCRIPTION = "procd is VIRTUAL-RUNTIME-init_manager"
 HOMEPAGE = "http://wiki.openwrt.org/doc/techref/procd"