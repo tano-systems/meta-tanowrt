tano-systems/meta-tanowrt

commit 57b04a45a21d9dae1b47f7dd2576bf52eecc461f

luci-base: Change order of luci-theme-initial script to zz
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

meta-tanowrt/recipes-extended/luci/luci-base.bb
@@ -5,7 +5,7 @@
 # Anton Kikin <a.kikin@tano-systems.com>
 #
 
-PR = "tano4"
+PR = "tano5"
 
 SUMMARY = "LuCI core libraries"
 LICENSE = "Apache-2.0"
@@ -78,7 +78,7 @@ do_install_append() {
 
 	# Configure initial mediaurlbase
 	install -d ${D}${sysconfdir}/uci-defaults
-	install -m 0755 ${WORKDIR}/99_luci-theme-initial ${D}${sysconfdir}/uci-defaults/99_luci-theme-initial
+	install -m 0755 ${WORKDIR}/99_luci-theme-initial ${D}${sysconfdir}/uci-defaults/zz_luci-theme-initial
 
 	MEDIAURLBASE_ESCAPED="${@d.getVar('LUCI_INITIAL_MEDIAURLBASE', True).replace('/', '\/')}"
 	sed -i -e "s/\(luci\.main\.mediaurlbase\)=.*/\1=${MEDIAURLBASE_ESCAPED}/" ${D}${sysconfdir}/uci-defaults/99_luci-theme-initial

commit 1e83e8414f2672af240f6e6d3bd8b8c9d1b2045f

luci-base: Fix luci-theme-initial script order change
Fixes: 57b04a4 ("luci-base: Change order of luci-theme-initial script to zz")
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

meta-tanowrt/recipes-extended/luci/luci-base.bb
@@ -5,7 +5,7 @@
 # Anton Kikin <a.kikin@tano-systems.com>
 #
 
-PR = "tano5"
+PR = "tano6"
 
 SUMMARY = "LuCI core libraries"
 LICENSE = "Apache-2.0"
@@ -81,7 +81,7 @@ do_install_append() {
 	install -m 0755 ${WORKDIR}/99_luci-theme-initial ${D}${sysconfdir}/uci-defaults/zz_luci-theme-initial
 
 	MEDIAURLBASE_ESCAPED="${@d.getVar('LUCI_INITIAL_MEDIAURLBASE', True).replace('/', '\/')}"
-	sed -i -e "s/\(luci\.main\.mediaurlbase\)=.*/\1=${MEDIAURLBASE_ESCAPED}/" ${D}${sysconfdir}/uci-defaults/99_luci-theme-initial
+	sed -i -e "s/\(luci\.main\.mediaurlbase\)=.*/\1=${MEDIAURLBASE_ESCAPED}/" ${D}${sysconfdir}/uci-defaults/zz_luci-theme-initial
 
 	if [ "${LUCI_INSTALL_LUASRC_PATH}" != "${libdir}/lua/luci" ]; then
 		# Make symlink for compatibility with upstream paths