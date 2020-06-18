tano-systems/meta-tanowrt

commit b9c3084b4c543b1c5ff5b4667fbdcdbb7fcfeabc

fstools: Update to the latest git head
8426903 blockd: fix segfault triggered by non-autofs mounts
eec16e2 blockd: add optional "device" parameter to "info" ubus method
9ab936d block(d): always call hotplug.d "mount" scripts from blockd
4963db4 blockd: use uloop_process for calling /sbin/hotplug-call mount
cddd902 Truncate FAT filesystem label until 1st occurance of a blank (0x20)
8b9e601 block: always use st_dev (device ID) of / when looking for root
37c9148 block: simplify check_extroot() a bit
d70774d block: add some basic extroot documentation
32db27d Revert "block: support hierarchical mount/umount"
0b93429 Revert "block: mount_action: handle mount/umount deps"
c80f700 libfstools/mtd: attempt to read from OOB data if empty space is found
830441d blockd: remove symlink linkpath file if it's a dir or link
d1f1f2b block: remove mount target file if it's a link
62c578c blockd: report "target" path as "mount" for autofs available mounts
84965b9 blockd: print symlink error code and string message
deb745f Revert "fstools: Add support to read-only MTD partitions (eg. recovery images)"

Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

meta-tanowrt/recipes-core/fstools/fstools/patches/0001-Define-GLOB_ONLYDIR-if-not-available.patch
@@ -1,4 +1,4 @@
-From b0d27a658d98acfc608878bc0e57d390c3ac4698 Mon Sep 17 00:00:00 2001
+From f76041604f5a28e1bcc28a2a168e91a40e1cab9d Mon Sep 17 00:00:00 2001
 From: Khem Raj <raj.khem@gmail.com>
 Date: Fri, 22 Jan 2016 05:07:43 +0000
 Subject: [PATCH 01/11] Define GLOB_ONLYDIR if not available
@@ -27,5 +27,5 @@ index 14214a3..9b79bb7 100644
  
  static bool keep_sysupgrade;
 -- 
-2.25.0.windows.1
+2.16.2.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools/patches/0002-block-Validate-libubi_open-return-value.patch
@@ -1,4 +1,4 @@
-From a2b27cd42c49ac7b998af51ebb9b766fd132dc7f Mon Sep 17 00:00:00 2001
+From 0a74b80f4329b0db7dc4a538a87f2dbdf38d955e Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Sun, 1 Jul 2018 05:17:30 +0300
 Subject: [PATCH 02/11] block: Validate libubi_open() return value
@@ -9,10 +9,10 @@ Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>
  1 file changed, 16 insertions(+), 14 deletions(-)
 
 diff --git a/block.c b/block.c
-index 50d877f..5606772 100644
+index 569bf56..e2ab381 100644
 --- a/block.c
 +++ b/block.c
-@@ -1643,22 +1643,24 @@ static int main_extroot(int argc, char **argv)
+@@ -1572,22 +1572,24 @@ static int main_extroot(int argc, char **argv)
  	/* ... but it also could be an UBI volume */
  	memset(blkdev_path, 0, sizeof(blkdev_path));
  	libubi = libubi_open();
@@ -50,7 +50,7 @@ index 50d877f..5606772 100644
 +	}
  #endif
  
- 	return mount_extroot(NULL);
+ 	/* As a last resort look for /etc/config/fstab on "rootfs" partition */
 -- 
-2.25.0.windows.1
+2.16.2.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools/patches/0003-ubi-Add-forgotten-newlines-to-error-messages.patch
@@ -1,4 +1,4 @@
-From 7b2fd8d07bd4900380d18d196309ce957afdd79e Mon Sep 17 00:00:00 2001
+From a8f6412e2fb105b25ab997b6a408abb647464a55 Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Sun, 1 Jul 2018 05:43:53 +0300
 Subject: [PATCH 03/11] ubi: Add forgotten newlines to error messages
@@ -95,5 +95,5 @@ index 54f67b8..0688d03 100644
  	}
  
 -- 
-2.25.0.windows.1
+2.16.2.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools/patches/0004-Fix-UUID-reading-from-vfat.patch
@@ -1,4 +1,4 @@
-From 06c00bfe417f9ba5073b23d95a60285178a1ffe4 Mon Sep 17 00:00:00 2001
+From 4c57a44f50ecbe3d9ef646304193133b847365e5 Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Thu, 11 Oct 2018 12:43:56 +0300
 Subject: [PATCH 04/11] Fix UUID reading from vfat
@@ -9,7 +9,7 @@ Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>
  1 file changed, 19 insertions(+), 7 deletions(-)
 
 diff --git a/libblkid-tiny/vfat.c b/libblkid-tiny/vfat.c
-index e70dd75..3129694 100644
+index 1bce002..99cc16f 100644
 --- a/libblkid-tiny/vfat.c
 +++ b/libblkid-tiny/vfat.c
 @@ -304,6 +304,12 @@ static int probe_vfat(blkid_probe pr, const struct blkid_idmag *mag)
@@ -87,5 +87,5 @@ index e70dd75..3129694 100644
  			struct fat32_fsinfo *fsinfo;
  
 -- 
-2.25.0.windows.1
+2.16.2.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools/patches/0005-block-Fix-some-memory-leaks-and-unnecessary-duplicat.patch
@@ -1,4 +1,4 @@
-From 146326dc439702382a1947caabe832d9bd84bf86 Mon Sep 17 00:00:00 2001
+From 8f7eafbbcb9a853bc4d2252ab07dfd93cef4aa3c Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Thu, 15 Nov 2018 16:23:23 +0300
 Subject: [PATCH 05/11] block: Fix some memory leaks and unnecessary duplicate
@@ -10,10 +10,10 @@ Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>
  1 file changed, 11 insertions(+)
 
 diff --git a/block.c b/block.c
-index 5606772..41997e6 100644
+index e2ab381..4e348a0 100644
 --- a/block.c
 +++ b/block.c
-@@ -1109,6 +1109,17 @@ static int mount_device(struct device *dev, int type)
+@@ -1041,6 +1041,17 @@ static int mount_device(struct probe_info *pr, int type)
  		return err;
  	}
  
@@ -29,8 +29,8 @@ index 5606772..41997e6 100644
 +		free(mp);
 +
  	if (type == TYPE_HOTPLUG)
- 		blockd_notify(device, m, pr);
+ 		blockd_notify("hotplug", device, m, pr);
  
 -- 
-2.25.0.windows.1
+2.16.2.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools/patches/0006-blockd-Fix-simultaneous-multiple-devices-mounts-hand.patch
@@ -1,4 +1,4 @@
-From 66afc171ac93fac7904daa97ccb0c6efcb53057d Mon Sep 17 00:00:00 2001
+From ae71a95e7a15a4308b3ba4cddb7c969cb9f6991c Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Thu, 15 Nov 2018 16:24:21 +0300
 Subject: [PATCH 06/11] blockd: Fix simultaneous multiple devices mounts
@@ -10,17 +10,17 @@ Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>
  1 file changed, 1 insertion(+)
 
 diff --git a/blockd.c b/blockd.c
-index e07530c..f7a7cd4 100644
+index f0062c6..7cf2831 100644
 --- a/blockd.c
 +++ b/blockd.c
-@@ -237,6 +237,7 @@ block_hotplug(struct ubus_context *ctx, struct ubus_object *obj,
+@@ -278,6 +278,7 @@ block_hotplug(struct ubus_context *ctx, struct ubus_object *obj,
  	if (!device)
  		return UBUS_STATUS_UNKNOWN_ERROR;
  
 +	/* vlist_update(&devices); */
  	if (data[MOUNT_REMOVE]) {
  		vlist_delete(&devices, &device->node);
- 	} else {
+ 
 -- 
-2.25.0.windows.1
+2.16.2.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools/patches/0007-block-Add-log-message-for-unmounted-device.patch
@@ -1,4 +1,4 @@
-From b869693c6af9516681e0c5662f2e43ec42c2a2db Mon Sep 17 00:00:00 2001
+From 8e2ce0e4e88522f74eee310a18e99273998973f4 Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Wed, 14 Aug 2019 23:03:57 +0300
 Subject: [PATCH 07/11] block: Add log message for unmounted device
@@ -9,10 +9,10 @@ Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>
  1 file changed, 4 insertions(+), 2 deletions(-)
 
 diff --git a/block.c b/block.c
-index 41997e6..7ed283d 100644
+index 4e348a0..0417ab4 100644
 --- a/block.c
 +++ b/block.c
-@@ -1101,7 +1101,7 @@ static int mount_device(struct device *dev, int type)
+@@ -1033,7 +1033,7 @@ static int mount_device(struct probe_info *pr, int type)
  	mp = find_mount_point(pr->dev);
  	if (mp) {
  		if (m && m->type == TYPE_MOUNT && strcmp(m->target, mp)) {
@@ -21,7 +21,7 @@ index 41997e6..7ed283d 100644
  			err = -1;
  		} else
  			err = 0;
-@@ -1171,6 +1171,8 @@ static int mount_device(struct device *dev, int type)
+@@ -1105,6 +1105,8 @@ static int mount_device(struct probe_info *pr, int type)
  		ULOG_ERR("mounting %s (%s) as %s failed (%d) - %m\n",
  				pr->dev, pr->type, target, errno);
  		return err;
@@ -30,7 +30,7 @@ index 41997e6..7ed283d 100644
  	}
  
  	handle_swapfiles(true);
-@@ -1200,7 +1202,7 @@ static int umount_device(char *path, int type, bool all)
+@@ -1134,7 +1136,7 @@ static int umount_device(char *path, int type, bool all)
  		ULOG_ERR("unmounting %s (%s) failed (%d) - %m\n", path, mp,
  			 errno);
  	} else {
@@ -40,5 +40,5 @@ index 41997e6..7ed283d 100644
  	}
  
 -- 
-2.25.0.windows.1
+2.16.2.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools/patches/0008-block-Fix-device-path-creation.patch
@@ -1,4 +1,4 @@
-From ea6f5432af2978a3e3ca2805bee6661cebbadf5e Mon Sep 17 00:00:00 2001
+From ffe1ab3e395d0c03e730bd26d047816d54509bf5 Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Wed, 14 Aug 2019 23:04:15 +0300
 Subject: [PATCH 08/11] block: Fix device path creation
@@ -9,14 +9,14 @@ it to the beginning of the device name.
 
 Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>
 ---
- block.c | 31 ++++++++++++-------------------
- 1 file changed, 12 insertions(+), 19 deletions(-)
+ block.c | 35 +++++++++++++++--------------------
+ 1 file changed, 15 insertions(+), 20 deletions(-)
 
 diff --git a/block.c b/block.c
-index 7ed283d..0fe02a5 100644
+index 0417ab4..3d1a9e1 100644
 --- a/block.c
 +++ b/block.c
-@@ -211,14 +211,6 @@ static char *blobmsg_get_strdup(struct blob_attr *attr)
+@@ -200,14 +200,6 @@ static char *blobmsg_get_strdup(struct blob_attr *attr)
  	return strdup(blobmsg_get_string(attr));
  }
  
@@ -31,7 +31,7 @@ index 7ed283d..0fe02a5 100644
  static void parse_mount_options(struct mount *m, char *optstr)
  {
  	int i;
-@@ -285,7 +277,7 @@ static int mount_add(struct uci_section *s)
+@@ -274,7 +266,7 @@ static int mount_add(struct uci_section *s)
  	m->uuid = blobmsg_get_strdup(tb[MOUNT_UUID]);
  	m->label = blobmsg_get_strdup(tb[MOUNT_LABEL]);
  	m->target = blobmsg_get_strdup(tb[MOUNT_TARGET]);
@@ -40,7 +40,7 @@ index 7ed283d..0fe02a5 100644
  	if (tb[MOUNT_AUTOFS])
  		m->autofs = blobmsg_get_u32(tb[MOUNT_AUTOFS]);
  	else
-@@ -332,7 +324,7 @@ static int swap_add(struct uci_section *s)
+@@ -321,7 +313,7 @@ static int swap_add(struct uci_section *s)
  	m->type = TYPE_SWAP;
  	m->uuid = blobmsg_get_strdup(tb[SWAP_UUID]);
  	m->label = blobmsg_get_strdup(tb[SWAP_LABEL]);
@@ -49,30 +49,21 @@ index 7ed283d..0fe02a5 100644
  	if (tb[SWAP_PRIO])
  		m->prio = blobmsg_get_u32(tb[SWAP_PRIO]);
  	if (m->prio)
-@@ -523,7 +515,7 @@ static int _cache_load(const char *path)
- 			dev = malloc(sizeof(struct device));
- 			dev->pr = pr;
- 			dev->m = find_block(pr->uuid, pr->label,
--					basename(pr->dev), NULL);
-+					pr->dev, NULL);
- 			vlist_add(&devices, &dev->node, dev);
- 		}
- 	}
-@@ -591,7 +583,7 @@ static struct device* find_block_device(char *uuid, char *label, char *path)
+@@ -552,7 +544,7 @@ static struct probe_info* find_block_info(char *uuid, char *label, char *path)
  
  	if (path)
- 		vlist_for_each_element(&devices, dev, node)
--			if (dev->pr->dev && !strcmp(basename(dev->pr->dev), basename(path)))
-+			if (dev->pr->dev && !strcmp(dev->pr->dev, path))
- 				return dev;
+ 		list_for_each_entry(pr, &devices, list)
+-			if (pr->dev && !strcmp(basename(pr->dev), basename(path)))
++			if (pr->dev && !strcmp(pr->dev, path))
+ 				return pr;
  
  	return NULL;
-@@ -1082,12 +1074,11 @@ static int mount_device(struct device *dev, int type)
+@@ -1014,19 +1006,17 @@ static int mount_device(struct probe_info *pr, int type)
+ 	if (!pr)
  		return -1;
  
- 	pr = dev->pr;
 -	device = basename(pr->dev);
- 
+-
  	if (!strcmp(pr->type, "swap")) {
  		if ((type == TYPE_HOTPLUG) && !auto_swap)
  			return -1;
@@ -81,53 +72,57 @@ index 7ed283d..0fe02a5 100644
  		if (m || anon_swap)
  			swapon(pr->dev, (m) ? (m->prio) : (0));
  
-@@ -1121,7 +1112,9 @@ static int mount_device(struct device *dev, int type)
+ 		return 0;
+ 	}
+ 
+-	m = find_block(pr->uuid, pr->label, device, NULL);
++	m = find_block(pr->uuid, pr->label, pr->dev, NULL);
+ 	if (m && m->extroot)
+ 		return -1;
+ 
+@@ -1053,7 +1043,9 @@ static int mount_device(struct probe_info *pr, int type)
  		free(mp);
  
  	if (type == TYPE_HOTPLUG)
--		blockd_notify(device, m, pr);
-+		blockd_notify(pr->dev, m, pr);
+-		blockd_notify("hotplug", device, m, pr);
++		blockd_notify("hotplug", pr->dev, m, pr);
 +
 +	device = basename(pr->dev);
  
  	/* Check if device should be mounted & set the target directory */
  	if (m) {
-@@ -1178,7 +1171,7 @@ static int mount_device(struct device *dev, int type)
+@@ -1112,7 +1104,7 @@ static int mount_device(struct probe_info *pr, int type)
  	handle_swapfiles(true);
  
  	if (type != TYPE_AUTOFS)
--		hotplug_call_mount("add", device);
-+		hotplug_call_mount("add", pr->dev);
+-		blockd_notify("mount", device, NULL, NULL);
++		blockd_notify("mount", pr->dev, NULL, NULL);
  
  	return 0;
  }
-@@ -1195,7 +1188,7 @@ static int umount_device(char *path, int type, bool all)
+@@ -1129,7 +1121,7 @@ static int umount_device(char *path, int type, bool all)
  		return 0;
  
  	if (type != TYPE_AUTOFS)
--		hotplug_call_mount("remove", basename(path));
-+		hotplug_call_mount("remove", path);
+-		blockd_notify("umount", basename(path), NULL, NULL);
++		blockd_notify("umount", path, NULL, NULL);
  
  	err = umount2(mp, MNT_DETACH);
  	if (err) {
-@@ -1229,7 +1222,7 @@ static int mount_action(char *action, char *device, int type)
- 			blockd_notify(device, NULL, NULL);
+@@ -1437,8 +1429,11 @@ static int check_extroot(char *path)
+ 			 errno);
+ 	fclose(fp);
  
- 		if (!the_dev || !the_dev->m || the_dev->m->type != TYPE_MOUNT) {
--			snprintf(path, sizeof(path), "/dev/%s", device);
-+			snprintf(path, sizeof(path), "%s", device);
- 			umount_device(path, type, true);
- 		} else
- 			vlist_for_element_to_last_reverse(&devices, the_dev, dev, node)
-@@ -1512,7 +1505,7 @@ static int check_extroot(char *path)
- 				return 0;
+-	if (*uuid && !strcasecmp(uuid, pr->uuid))
+-		return 0;
++	if (*uuid && !strcasecmp(uuid, pr->uuid)) {
++		ULOG_ERR("extroot: UUID mismatch (root: %s, %s: %s)\n",
++		         pr->uuid, path, uuid);
++		return -1;
++	}
  
- 			ULOG_ERR("extroot: UUID mismatch (root: %s, %s: %s)\n",
--			         pr->uuid, basename(path), uuid);
-+			         pr->uuid, path, uuid);
- 			return -1;
- 		}
- 	}
+ 	ULOG_ERR("extroot: UUID mismatch (root: %s, %s: %s)\n", pr->uuid,
+ 		 basename(path), uuid);
 -- 
-2.25.0.windows.1
+2.16.2.windows.1
 

commit 3dfb5bee15a3dc4563b0f5138d88c9dabc08d101

fstools: Update patches
Fixes: b9c3084 ("fstools: Update to the latest git head")
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

meta-tanowrt/recipes-core/fstools/fstools/patches/0001-Define-GLOB_ONLYDIR-if-not-available.patch
@@ -1,4 +1,4 @@
-From f76041604f5a28e1bcc28a2a168e91a40e1cab9d Mon Sep 17 00:00:00 2001
+From 2e6bd05a537175d0c9abb4215d42272e35d2fcc0 Mon Sep 17 00:00:00 2001
 From: Khem Raj <raj.khem@gmail.com>
 Date: Fri, 22 Jan 2016 05:07:43 +0000
 Subject: [PATCH 01/11] Define GLOB_ONLYDIR if not available
@@ -27,5 +27,5 @@ index 14214a3..9b79bb7 100644
  
  static bool keep_sysupgrade;
 -- 
-2.16.2.windows.1
+2.25.0.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools/patches/0002-block-Validate-libubi_open-return-value.patch
@@ -1,4 +1,4 @@
-From 0a74b80f4329b0db7dc4a538a87f2dbdf38d955e Mon Sep 17 00:00:00 2001
+From e250624000025b0a6e89d7a9952ba8f794c50ea0 Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Sun, 1 Jul 2018 05:17:30 +0300
 Subject: [PATCH 02/11] block: Validate libubi_open() return value
@@ -52,5 +52,5 @@ index 569bf56..e2ab381 100644
  
  	/* As a last resort look for /etc/config/fstab on "rootfs" partition */
 -- 
-2.16.2.windows.1
+2.25.0.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools/patches/0003-ubi-Add-forgotten-newlines-to-error-messages.patch
@@ -1,4 +1,4 @@
-From a8f6412e2fb105b25ab997b6a408abb647464a55 Mon Sep 17 00:00:00 2001
+From 882575d241817a3d721e835ee333e3a2734acc2e Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Sun, 1 Jul 2018 05:43:53 +0300
 Subject: [PATCH 03/11] ubi: Add forgotten newlines to error messages
@@ -95,5 +95,5 @@ index 54f67b8..0688d03 100644
  	}
  
 -- 
-2.16.2.windows.1
+2.25.0.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools/patches/0004-Fix-UUID-reading-from-vfat.patch
@@ -1,4 +1,4 @@
-From 4c57a44f50ecbe3d9ef646304193133b847365e5 Mon Sep 17 00:00:00 2001
+From a8cdb1d2118066b0be808e6997b3222a6ff66374 Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Thu, 11 Oct 2018 12:43:56 +0300
 Subject: [PATCH 04/11] Fix UUID reading from vfat
@@ -87,5 +87,5 @@ index 1bce002..99cc16f 100644
  			struct fat32_fsinfo *fsinfo;
  
 -- 
-2.16.2.windows.1
+2.25.0.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools/patches/0005-block-Fix-some-memory-leaks-and-unnecessary-duplicat.patch
@@ -1,4 +1,4 @@
-From 8f7eafbbcb9a853bc4d2252ab07dfd93cef4aa3c Mon Sep 17 00:00:00 2001
+From b5d22f2a2f013af88ed74d36b066111f05679ec4 Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Thu, 15 Nov 2018 16:23:23 +0300
 Subject: [PATCH 05/11] block: Fix some memory leaks and unnecessary duplicate
@@ -32,5 +32,5 @@ index e2ab381..4e348a0 100644
  		blockd_notify("hotplug", device, m, pr);
  
 -- 
-2.16.2.windows.1
+2.25.0.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools/patches/0006-blockd-Fix-simultaneous-multiple-devices-mounts-hand.patch
@@ -1,4 +1,4 @@
-From ae71a95e7a15a4308b3ba4cddb7c969cb9f6991c Mon Sep 17 00:00:00 2001
+From b22a9d26b54940591555958af5b5ae609ebc56ff Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Thu, 15 Nov 2018 16:24:21 +0300
 Subject: [PATCH 06/11] blockd: Fix simultaneous multiple devices mounts
@@ -22,5 +22,5 @@ index f0062c6..7cf2831 100644
  		vlist_delete(&devices, &device->node);
  
 -- 
-2.16.2.windows.1
+2.25.0.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools/patches/0007-block-Add-log-message-for-unmounted-device.patch
@@ -1,4 +1,4 @@
-From 8e2ce0e4e88522f74eee310a18e99273998973f4 Mon Sep 17 00:00:00 2001
+From 2f85c6363ca5fd8e769462516f6b047c21a75ca9 Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Wed, 14 Aug 2019 23:03:57 +0300
 Subject: [PATCH 07/11] block: Add log message for unmounted device
@@ -40,5 +40,5 @@ index 4e348a0..0417ab4 100644
  	}
  
 -- 
-2.16.2.windows.1
+2.25.0.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools/patches/0008-block-Fix-device-path-creation.patch
@@ -1,4 +1,4 @@
-From ffe1ab3e395d0c03e730bd26d047816d54509bf5 Mon Sep 17 00:00:00 2001
+From 3b5488cdd6540b6d6fdbdf6ea2512782bc438ca0 Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Wed, 14 Aug 2019 23:04:15 +0300
 Subject: [PATCH 08/11] block: Fix device path creation
@@ -9,11 +9,11 @@ it to the beginning of the device name.
 
 Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>
 ---
- block.c | 35 +++++++++++++++--------------------
- 1 file changed, 15 insertions(+), 20 deletions(-)
+ block.c | 37 ++++++++++++++++---------------------
+ 1 file changed, 16 insertions(+), 21 deletions(-)
 
 diff --git a/block.c b/block.c
-index 0417ab4..3d1a9e1 100644
+index 0417ab4..4ea2363 100644
 --- a/block.c
 +++ b/block.c
 @@ -200,14 +200,6 @@ static char *blobmsg_get_strdup(struct blob_attr *attr)
@@ -109,6 +109,15 @@ index 0417ab4..3d1a9e1 100644
  
  	err = umount2(mp, MNT_DETACH);
  	if (err) {
+@@ -1150,7 +1142,7 @@ static int mount_action(char *action, char *device, int type)
+ 
+ 	if (!action || !device)
+ 		return -1;
+-	snprintf(path, sizeof(path), "/dev/%s", device);
++	snprintf(path, sizeof(path), "%s", device);
+ 
+ 	if (!strcmp(action, "remove")) {
+ 		if (type == TYPE_HOTPLUG)
 @@ -1437,8 +1429,11 @@ static int check_extroot(char *path)
  			 errno);
  	fclose(fp);
@@ -124,5 +133,5 @@ index 0417ab4..3d1a9e1 100644
  	ULOG_ERR("extroot: UUID mismatch (root: %s, %s: %s)\n", pr->uuid,
  		 basename(path), uuid);
 -- 
-2.16.2.windows.1
+2.25.0.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools/patches/0009-libfstools-Support-for-f2fs-ext4-overlay-on-a-separa.patch
@@ -1,4 +1,4 @@
-From 48dd7f25164cb5680987bc32a3669f3a7b5aa406 Mon Sep 17 00:00:00 2001
+From ee8f6ec3ea186c9a898266a568967cc69c117641 Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Fri, 16 Aug 2019 19:50:29 +0300
 Subject: [PATCH 09/11] libfstools: Support for f2fs/ext4 overlay on a separate
@@ -10,7 +10,7 @@ same disk that is used for the root filesystem.
 Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>
 ---
  CMakeLists.txt        |   8 +
- libfstools/rootdisk.c | 519 ++++++++++++++++++++++++++++++++++++++++++++++++--
+ libfstools/rootdisk.c | 519 ++++++++++++++++++++++++++++++++++++++++--
  mount_root.c          |   4 +
  3 files changed, 512 insertions(+), 19 deletions(-)
 
@@ -702,5 +702,5 @@ index dffb0a6..f8aa5e1 100644
  	case FS_UBIFS:
  		fs_state_set("/overlay", FS_STATE_READY);
 -- 
-2.16.2.windows.1
+2.25.0.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools/patches/0010-fstools-Configurable-overlay-partition-name.patch
@@ -1,19 +1,19 @@
-From 487eb31baedfdc394d2af9c8c49a8a1e023d9998 Mon Sep 17 00:00:00 2001
+From 1b2e47effab8cc5844049f03789938cb8cfadf2f Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Mon, 16 Dec 2019 03:58:26 +0300
 Subject: [PATCH 10/11] fstools: Configurable overlay partition name
 
 Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>
 ---
- CMakeLists.txt        | 13 +++++++------
- block.c               | 10 +++++++---
- jffs2reset.c          |  8 ++++----
- libfstools/overlay.c  |  5 +++--
- libfstools/rootdisk.c | 11 ++++++-----
- mount_root.c          | 11 ++++++-----
- overlay_partition.c   | 54 +++++++++++++++++++++++++++++++++++++++++++++++++++
- overlay_partition.h   | 19 ++++++++++++++++++
- snapshot.c            | 15 +++++++-------
+ CMakeLists.txt        | 13 ++++++-----
+ block.c               | 10 +++++---
+ jffs2reset.c          |  8 +++----
+ libfstools/overlay.c  |  5 ++--
+ libfstools/rootdisk.c | 11 +++++----
+ mount_root.c          | 11 +++++----
+ overlay_partition.c   | 54 +++++++++++++++++++++++++++++++++++++++++++
+ overlay_partition.h   | 19 +++++++++++++++
+ snapshot.c            | 15 ++++++------
  ubi.c                 |  3 ++-
  10 files changed, 116 insertions(+), 33 deletions(-)
  create mode 100644 overlay_partition.c
@@ -70,7 +70,7 @@ index cf55d89..2735bdd 100644
  TARGET_LINK_LIBRARIES(ubi ubi-utils ubox)
  INSTALL(TARGETS ubi RUNTIME DESTINATION sbin)
 diff --git a/block.c b/block.c
-index 3d1a9e1..c8c5fde 100644
+index 4ea2363..fe96828 100644
 --- a/block.c
 +++ b/block.c
 @@ -45,6 +45,7 @@
@@ -462,5 +462,5 @@ index 0688d03..cc58f9e 100644
  	} else if (!strcmp(argv[1], "detach")) {
  		return main_detach(argv[2]);
 -- 
-2.16.2.windows.1
+2.25.0.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools/patches/0011-libfstools-Fix-64-bit-compilation-issues.patch
@@ -1,4 +1,4 @@
-From d86acc0e4265773d23c6c9bca617bbd2876443e3 Mon Sep 17 00:00:00 2001
+From 23224b9b4fcb0add5249309a186e7fe6e0fddc14 Mon Sep 17 00:00:00 2001
 From: Anton Kikin <a.kikin@tano-systems.com>
 Date: Fri, 27 Dec 2019 04:06:44 +0300
 Subject: [PATCH 11/11] libfstools: Fix 64-bit compilation issues
@@ -47,5 +47,5 @@ index 4b8ffee..05aa43e 100644
  			snprintf(str, sizeof(str), "mkfs.ext4 -q -L %s %s", get_overlay_partition(), v->blk);
  		}
 -- 
-2.16.2.windows.1
+2.25.0.windows.1
 

meta-tanowrt/recipes-core/fstools/fstools_git.bb
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-2020 Anton Kikin <a.kikin@tano-systems.com>
 # Released under the MIT license (see COPYING.MIT for the terms)
 
-PR = "tano28"
+PR = "tano29"
 
 DESCRIPTION = "OpenWrt filesystem utilities"
 HOMEPAGE = "https://git.openwrt.org/?p=project/fstools.git;a=summary"