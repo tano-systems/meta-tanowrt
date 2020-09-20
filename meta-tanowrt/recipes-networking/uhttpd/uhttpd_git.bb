tano-systems/meta-tanowrt

commit 5563c8eb142632cebf4470b24c5061bdefc4964c

uhttpd: Update to the latest git head
47c34bd ubus: add ACL support for "subscribe" request
1172357 ubus: add new RESTful API

Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

meta-tanowrt/recipes-networking/uhttpd/uhttpd_git.bb
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-2020 Anton Kikin <a.kikin@tano-systems.com>
 # Released under the MIT license (see COPYING.MIT for the terms)
 
-PR = "tano25"
+PR = "tano26"
 DESCRIPTION = "Tiny HTTP server"
 HOMEPAGE = "http://git.openwrt.org/?p=project/uhttpd.git;a=summary"
 LICENSE = "BSD"
@@ -26,9 +26,9 @@ SRC_URI = "\
 PROVIDES += "uhttpd-mod-ubus uhttpd-mod-lua"
 RPROVIDES_${PN} += "uhttpd-mod-ubus uhttpd-mod-lua"
 
-# 14.09.2020
-# ubus: fix blob_buf initialization
-SRCREV = "fe1888f19e7f8ee4237409b8616c82926190c2f8"
+# 18.09.2020
+# ubus: add ACL support for "subscribe" request
+SRCREV = "47c34bd6ad49cae408b8d7c150c6f9f324aaddf5"
 S = "${WORKDIR}/git"
 
 inherit cmake pkgconfig tanowrt-services tanowrt-lua

commit 2f101bf4dd085979a4d3dd7707e0a2c520f0a16c

uhttpd: Fix ubus empty responses format
After the last update of uhttpd, the format of empty responses for
ubus requests was changed. This led to some unexpected results in LuCI.
See comments in added patch for details.

Fixes: 5563c8eb ("uhttpd: Update to the latest git head")
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

meta-tanowrt/recipes-networking/uhttpd/uhttpd/patches/0202-ubus-fix-format-for-empty-responses.patch
@@ -1,0 +1,55 @@
+From 81dfb323aa7ef4392b18cfa165e80e7bca72f5a4 Mon Sep 17 00:00:00 2001
+From: Anton Kikin <a.kikin@tano-systems.com>
+Date: Sun, 20 Sep 2020 03:27:34 +0300
+Subject: [PATCH] ubus: fix format for empty responses
+
+After commit 1172357, empty responses are formatted as an empty object
+after result status in result array:
+
+    [{"jsonrpc":"2.0","id":25,"result":[0,{}]}]
+
+Previously, only the status value was written to result array
+for empty responses:
+
+    [{"jsonrpc":"2.0","id":25,"result":[0]}]
+
+This commit reverts behavior to what it was before commit 1172357
+for legacy ubus requests. The reason is that LuCI code for some ubus
+requests (e.g. uci.apply) does not expect to find an empty object in
+the 'result' array for successful responses. In LuCI code, if the
+result array has more than one argument, it returns the second element
+as a response to the user code (see handleCallReply method in rpc.js
+from luci-base module). While the user code expects to see the status
+code as a response, it gets an empty object.
+
+Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>
+---
+ ubus.c | 12 ++++++++----
+ 1 file changed, 8 insertions(+), 4 deletions(-)
+
+diff --git a/ubus.c b/ubus.c
+index 1cf5c5f..3a52bc6 100644
+--- a/ubus.c
++++ b/ubus.c
+@@ -465,10 +465,14 @@ uh_ubus_request_cb(struct ubus_request *req, int ret)
+ 		uh_ubus_init_json_rpc_response(cl, &buf);
+ 		r = blobmsg_open_array(&buf, "result");
+ 		blobmsg_add_u32(&buf, "", ret);
+-		c = blobmsg_open_table(&buf, NULL);
+-		blob_for_each_attr(cur, du->buf.head, rem)
+-			blobmsg_add_blob(&buf, cur);
+-		blobmsg_close_table(&buf, c);
++
++		if (blob_len(du->buf.head)) {
++			c = blobmsg_open_table(&buf, NULL);
++			blob_for_each_attr(cur, du->buf.head, rem)
++				blobmsg_add_blob(&buf, cur);
++			blobmsg_close_table(&buf, c);
++		}
++
+ 		blobmsg_close_array(&buf, r);
+ 		uh_ubus_send_response(cl, &buf);
+ 		return;
+-- 
+2.16.2.windows.1
+

meta-tanowrt/recipes-networking/uhttpd/uhttpd_git.bb
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-2020 Anton Kikin <a.kikin@tano-systems.com>
 # Released under the MIT license (see COPYING.MIT for the terms)
 
-PR = "tano26"
+PR = "tano27"
 DESCRIPTION = "Tiny HTTP server"
 HOMEPAGE = "http://git.openwrt.org/?p=project/uhttpd.git;a=summary"
 LICENSE = "BSD"
@@ -17,6 +17,7 @@ SRC_URI = "\
 	file://0100-fix-wrong-binaries-found-due-to-inconsistent-path.patch \
 	file://0200-add-gz-support.patch \
 	file://0201-fix-uh_file_mime_lookup.patch \
+	file://0202-ubus-fix-format-for-empty-responses.patch \
 	file://uhttpd.config \
 	file://uhttpd.init \
 	file://ubus.default \