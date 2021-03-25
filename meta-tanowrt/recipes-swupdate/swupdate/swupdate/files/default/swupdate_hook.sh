cf4148b879be1015d0363c69ec7c31bbc495a55f
Anton Kikin <a.kikin@tano-systems.com>
2021-03-26 02:52:13 +0300

    swupdate: Run scripts from /usr/lib/swupdate/hook.d on hook events
    Modified the default 'swupdate_hook.sh' script to sequentially
    run user/custom scripts from the '/usr/lib/swupdate/hook.d' folder.
    
    Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

--- meta-tanowrt/recipes-swupdate/swupdate/swupdate/files/default/swupdate_hook.sh
+++ meta-tanowrt/recipes-swupdate/swupdate/swupdate/files/default/swupdate_hook.sh
@@ -9,27 +9,16 @@
 . /usr/lib/swupdate/swupdate.sh
 
 case "$1" in
-	preupdate)
-		if [ "${SWU_SOFTWARE_SET}" = "tanowrt" ] && \
-		   [ "${SWU_RUNNING_MODE}" = "system-a" ] || \
-		   [ "${SWU_RUNNING_MODE}" = "system-b" ]; then
-			echo 1 > "${SWU_REBOOT_STATE_FILE}"
-			echo "Rebooting enabled"
-		else
-			echo 0 > "${SWU_REBOOT_STATE_FILE}"
-			echo "Rebooting disabled"
-		fi
-		;;
-
-	postupdate-success)
-		REBOOT=$(cat "${SWU_REBOOT_STATE_FILE}" 2> /dev/null || echo 0)
-		if [ "${REBOOT}" = "1" ]; then
-			echo "Rebooting in 3 seconds..."
-			ubus call system reboot "{ 'delay': 3 }"
-		fi
-		;;
-
-	postupdate-failed)
+	preupdate|postupdate-success|postupdate-failed)
+		cd /usr/lib/swupdate/hook.d 2>/dev/null || return 0
+		scripts="$(ls)"
+		[ -z "$scripts" ] && exit 0
+		for script in $scripts; do
+			if [ -x "$script" ]; then
+				./${script} "${1}"
+				[ "$?" = "0" ] || exit 0
+			fi
+		done
 		;;
 
 	*)
