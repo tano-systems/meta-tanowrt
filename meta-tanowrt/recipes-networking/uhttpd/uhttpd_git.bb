tano-systems/meta-tanowrt

commit b9fe2b5a3193228ec0efc0b714e9ad7cf214415d

uhttpd: Fix MIME type detection for files with dots in name
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

meta-tanowrt/recipes-networking/uhttpd/uhttpd_git.bb
@@ -5,7 +5,7 @@
 # Copyright (C) 2018-2021 Anton Kikin <a.kikin@tano-systems.com>
 #
 
-PR = "tano35"
+PR = "tano36"
 DESCRIPTION = "Tiny HTTP server"
 HOMEPAGE = "http://git.openwrt.org/?p=project/uhttpd.git;a=summary"
 LICENSE = "BSD"
@@ -32,6 +32,8 @@ SRC_URI += "\
 	file://1003-ubus-Unsubscribe-on-client-request-network-timeout.patch \
 	file://1004-main-Fix-formatting-in-usage-help-text.patch \
 	file://1005-ubus-Disable-timeout-for-ubus-subscription-connectio.patch \
+	file://1006-Fix-MIME-type-detection-for-files-with-dots-in-name.patch \
+	file://patch.patch \
 "
 
 PROVIDES += "uhttpd-mod-ubus uhttpd-mod-lua"

meta-tanowrt/recipes-networking/uhttpd/uhttpd/patches/1006-Fix-MIME-type-detection-for-files-with-dots-in-name.patch
@@ -1,0 +1,75 @@
+From 7325b76b40163a749e4cc7b35c5f0def45c70417 Mon Sep 17 00:00:00 2001
+From: Anton Kikin <a.kikin@tano-systems.com>
+Date: Mon, 21 Jun 2021 17:59:28 +0300
+Subject: [PATCH] Fix MIME type detection for files with dots in name
+
+Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>
+---
+ file.c | 36 ++++++++++++++++++++----------------
+ 1 file changed, 20 insertions(+), 16 deletions(-)
+
+diff --git a/file.c b/file.c
+index abb5edc..4e3eb62 100644
+--- a/file.c
++++ b/file.c
+@@ -288,37 +288,41 @@ uh_path_lookup(struct client *cl, const char *url)
+ 
+ static const char * uh_file_mime_lookup(const char *path)
+ {
+-	const struct mimetype *m = &uh_mime_types[0];
++	const struct mimetype *m;
+ 
+ 	const char *ext;
+ 	size_t extlen = 0;
+ 	int gz = 0;
+-	
+-	if (!(ext = strrchr(path, '/')))
+-		return 0;
+ 
+-	if (!(ext = strchr(ext, '.')))
++	if (!(ext = strrchr(path, '/')))
+ 		return 0;
+ 
+ 	if (strcasecmp(strrchr(ext, '.'), ".gz") == 0)
+ 		gz = 1;
+ 
+-	/* Skip dot in extension */
+-	ext++;
++	while (ext) {
++		if (!(ext = strchr(ext, '.')))
++			return 0;
+ 
+-	extlen = strlen(ext);
++		/* Skip dot in extension */
++		ext++;
+ 
+-	while (m->extn) {
+-		if (strcasecmp(ext, m->extn) == 0) {
+-			return m->mime;
+-		}
+-		else if (gz) {
+-			if ((extlen >= 3) && (strncasecmp(ext, m->extn, extlen - 3) == 0)) {
++		extlen = strlen(ext);
++
++		m = &uh_mime_types[0];
++
++		while (m->extn) {
++			if (strcasecmp(ext, m->extn) == 0) {
+ 				return m->mime;
+ 			}
+-		}
++			else if (gz) {
++				if ((extlen >= 3) && (strncasecmp(ext, m->extn, extlen - 3) == 0)) {
++					return m->mime;
++				}
++			}
+ 
+-		m++;
++			m++;
++		}
+ 	}
+ 
+ 	return 0;
+-- 
+2.28.0.windows.1
+

commit 7b73a10d77d3dc1f6bf26004305337af409b3089

uhttpd: Remove reference to missed patch
Fixes: b9fe2b5a ("uhttpd: Fix MIME type detection for files with dots in name")

Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

meta-tanowrt/recipes-networking/uhttpd/uhttpd_git.bb
@@ -5,7 +5,7 @@
 # Copyright (C) 2018-2021 Anton Kikin <a.kikin@tano-systems.com>
 #
 
-PR = "tano36"
+PR = "tano37"
 DESCRIPTION = "Tiny HTTP server"
 HOMEPAGE = "http://git.openwrt.org/?p=project/uhttpd.git;a=summary"
 LICENSE = "BSD"
@@ -33,7 +33,6 @@ SRC_URI += "\
 	file://1004-main-Fix-formatting-in-usage-help-text.patch \
 	file://1005-ubus-Disable-timeout-for-ubus-subscription-connectio.patch \
 	file://1006-Fix-MIME-type-detection-for-files-with-dots-in-name.patch \
-	file://patch.patch \
 "
 
 PROVIDES += "uhttpd-mod-ubus uhttpd-mod-lua"