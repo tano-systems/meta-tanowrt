tano-systems/meta-tanowrt

commit c82c5c35eea2455dad4fe3544464bcbd34d66d97

tanowrt-kmods: Fix for missing /lib/modules case
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

meta-tanowrt/classes/tanowrt-kmods.bbclass
@@ -17,6 +17,8 @@ MODULES_FLATTEN_PATHS ?= "\
 "
 
 rootfs_flatten_modules_hook() {
+    [ -d "${IMAGE_ROOTFS}/lib/modules" ] || return
+
     KERNEL_DIR="${KERNEL_VERSION}"
 
     if [ -n "${KERNEL_DIR}" ] && [ ! -d "${IMAGE_ROOTFS}/lib/modules/${KERNEL_DIR}" ]; then

commit 500bbf5ac543ed8e5130cfcfbfa780ea64e90578

tanowrt-kmods: Fix return statement
When using return without argument, the function exits
with error code 1. Explicitly specify the argument,
for the successful function exit.

Fixes: c82c5c35 ("tanowrt-kmods: Fix for missing /lib/modules case")
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

meta-tanowrt/classes/tanowrt-kmods.bbclass
@@ -17,7 +17,7 @@ MODULES_FLATTEN_PATHS ?= "\
 "
 
 rootfs_flatten_modules_hook() {
-    [ -d "${IMAGE_ROOTFS}/lib/modules" ] || return
+    [ -d "${IMAGE_ROOTFS}/lib/modules" ] || return 0
 
     KERNEL_DIR="${KERNEL_VERSION}"
 